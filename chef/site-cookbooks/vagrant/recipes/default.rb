e1441ca2ef6c8ae5e6f74d31c0525f3ac3331b89
pli888 <peter@gigasciencejournal.com>
2017-08-29 14:09:17 +0800

    Add vagrant Chef cookbook

---
+++ chef/site-cookbooks/vagrant/recipes/default.rb
@@ -0,0 +1,24 @@
+#
+# Cookbook Name:: vagrant
+# Recipe:: default
+#
+# Copyright 2012, Cogini
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+include_recipe "cuddle"
+
+log 'message' do
+    message 'Deploying CUDDEL server'
+    level :info
+
+['vim', 'tree'].each do |pkg|
+    package pkg
+end
+
+# iptables not required for default development environment
+service 'iptables' do
+    action [:disable, :stop]
+end
