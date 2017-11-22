c30813e368c655bb878cf06806be783c31afb568
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2017-11-22 16:07:15 +0800

    Added publication info, updated study contacts.

--- isa_validation.py
+++ isa_validation.py
@@ -1,4 +1,8 @@
+## Validation of ISA-Tab metadata produced manually and available in folder 'metadata-isa'
+
 import os
+import logging
 from isatools import isatab
 
-my_json_report = isatab.validate(open(os.path.join('./metadata-isa/', 'i_gsk_longitudinal.txt')))
+my_json_report = isatab.validate(open(os.path.join('./metadata-isa/', 'i_gsk_longitudinal.txt')), log_level=logging.WARN)
+
