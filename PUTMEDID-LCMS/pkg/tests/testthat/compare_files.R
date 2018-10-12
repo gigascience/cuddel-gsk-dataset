03d3e8f58058532f4878333af27d988b20244d5f
Peter Li <peter@gigasciencejournal.com>
2018-10-12 09:42:13 +0800

    Tidy up PUTMEDID-LCMS files

---
+++ PUTMEDID-LCMS/pkg/tests/testthat/compare_files.R
@@ -0,0 +1,53 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: peterli
+# Created on: 8/10/2018
+
+library(diffobj)
+#
+# filenameForA <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/PeakLabel_R.csv"
+# filenameForB <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/PeakLabel.csv"
+# all.equal(readLines(filenameForA), readLines(filenameForB))
+#
+# filenameForA <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/sortPeaks_R.csv"
+# filenameForB <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/sortPeaks.csv"
+# diffFile(filenameForA, filenameForB)
+#
+# filenameForA <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/pval1_R.csv"
+# filenameForB <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/pval1.csv"
+# diffFile(filenameForA, filenameForB)
+
+# sortPeaks_R <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/sortPeaks_R.csv"
+# sortPeaks_bshell <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/sortPeaks.csv"
+# diffFile(sortPeaks_R, sortPeaks_bshell)
+# all.equal(readLines(sortPeaks_R), readLines(sortPeaks_bshell))
+
+# idx1_R <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/idx1_R.csv"
+# idx1_bshell <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/idx1.csv"
+# diffFile(idx1_R, idx1_bshell)
+# all.equal(readLines(idx1_R), readLines(idx1_bshell))
+
+# oidx1_R <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/oidx1_R.csv"
+# oidx1_bshell <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/oidx1.csv"
+# diffFile(oidx1_R, oidx1_bshell)
+# all.equal(readLines(oidx1_R), readLines(oidx1_bshell))
+
+# Pidx1_R <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/Pidx1_R.csv"
+# Pidx1 <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/Pidx1.csv"
+# diffFile(Pidx1_R, Pidx1)
+# all.equal(readLines(Pidx1_R), readLines(Pidx1))
+
+# finCorrs_R <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/finCorrs_R.csv"
+# finCorrs <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/finCorrs.csv"
+# diffFile(finCorrs_R, finCorrs)
+# all.equal(readLines(finCorrs_R), readLines(finCorrs))
+
+# totCorrs_R <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/totCorrs_R.csv"
+# totCorrs <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/totCorrs.csv"
+# diffFile(totCorrs_R, totCorrs)
+# all.equal(readLines(totCorrs_R), readLines(totCorrs))
+
+Gcount_R <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID/tests/testthat/Gcount_R.csv"
+Gcount <- "/Users/peterli/PhpstormProjects/cuddel-gsk-dataset/PUTMEDID-LCMS/Gcount.csv"
+diffFile(Gcount_R, Gcount)
+all.equal(readLines(Gcount_R), readLines(Gcount))
