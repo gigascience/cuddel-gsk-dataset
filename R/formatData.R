a714f14d125f74da8ced33174da0a6b20a2e78ed
Peter Li <peter@gigasciencejournal.com>
2018-10-18 18:04:00 +0800

    Move data formatting out of univariate.R

--- R/formatData.R
+++ R/formatData.R
@@ -21,9 +21,17 @@ pos_cols <- grep('_pos_', colnames, value=TRUE)
 # Sort out feature variables ID
 dataMatrix <- paste0("P", rownames(pos_tp0_peaklist))
 # Create dataMatrix for positive-mode data
-pos_dataMatrix <- cbind(dataMatrix, pos_tp0_peaklist[, pos_cols])
+pos_dataMatrix <- cbind(pos_tp0_peaklist[, pos_cols])
 # Sort out rownames
-rownames(pos_dataMatrix) <- seq(1, nrow(pos_dataMatrix))
+rownames(pos_dataMatrix) <- dataMatrix
+
+# Output data
+write.table(
+    pos_dataMatrix,
+    file="dataMatrix.tsv",
+    sep="\t",
+    row.names=TRUE,
+    col.names=TRUE)
 
 # Create variableMetadata for positive-mode data
 # Define columns not to extract
@@ -33,23 +41,38 @@ variableMetadata <- paste0("P", rownames(pos_tp0_peaklist))
 # Read in pre-treated positive mode data
 pos_xcms_peaklist <- read.csv(paste(output_path, "xcms_pos_peaklist.csv", sep="/"))
 # Create variableMetadata matrix for positive-mode data
-pos_variableMetadata <- cbind(variableMetadata, pos_xcms_peaklist[rownames(pos_tp0_peaklist), meta_cols])
+pos_variableMetadata <- cbind(pos_xcms_peaklist[rownames(pos_tp0_peaklist), meta_cols])
 # Sort out rownames
-rownames(pos_variableMetadata) <- seq(1, nrow(pos_dataMatrix))
+rownames(pos_variableMetadata) <- variableMetadata
+# Output data
+write.table(
+    pos_variableMetadata,
+    file="variableMetadata.tsv",
+    sep="\t",
+    row.names=TRUE,
+    col.names=TRUE)
 
 # Create sampleMetadata for positive-mode data
 # Read in metadata from file
 meta <- read.csv(paste0(datadir, "gsk/meta/meta_20180831.csv"))
 meta <- meta[which(meta[, "file_name_pos"] %in% colnames), ]
 meta_colnames <- colnames(meta)
-req_cols <- c("file_name_pos", "sample_ID", "order", "type", "block", "Regimen", "food", "exercise")
-meta <- meta[, req_cols]
+req_cols <- c("sample_ID", "order", "type", "block", "Regimen", "food", "exercise", "Timepoint")
+pos_sampleMetadata <- meta[, req_cols]
 # Correct column rownames
-colnames(meta)[3] <- "injectionOrder"
-colnames(meta)[4] <- "sampleType"
-colnames(meta)[5] <- "batch"
-colnames(meta)[6] <- "regimen"
-mode <- replicate(nrow(meta), "pos")
-meta <- cbind(meta, mode)
+colnames(pos_sampleMetadata)[2] <- "injectionOrder"
+colnames(pos_sampleMetadata)[3] <- "sampleType"
+colnames(pos_sampleMetadata)[4] <- "batch"
+colnames(pos_sampleMetadata)[5] <- "regimen"
+colnames(pos_sampleMetadata)[8] <- "timepoint"
+mode <- replicate(nrow(pos_sampleMetadata), "pos")
+pos_sampleMetadata <- cbind(pos_sampleMetadata, mode)
 colnames(meta)[1] <- "sampleMetadata"
-rownames(meta) <- seq(1, nrow(meta))
+rownames(pos_sampleMetadata) <- meta[,"file_name_pos"]
+# Output data
+write.table(
+    pos_sampleMetadata,
+    file="sampleMetadata.tsv",
+    sep="\t",
+    row.names=TRUE,
+    col.names=TRUE)
--- R/univariate.R
+++ R/univariate.R
@@ -12,69 +12,12 @@ pos_dir = paste0(datadir, "gsk/raw/esi_pos/netcdf")
 # Output directory
 output_path <- paste0(pos_dir, "/output")
 
-# Create dataMatrix for positive-mode data
-# Read in pre-treated positive mode data
-pos_tp0_peaklist <- read.csv(paste(output_path, "pos_tp0_norm_plasma_peaklist.csv", sep="/"))
-colnames <- colnames(pos_tp0_peaklist)
-pos_cols <- grep('_pos_', colnames, value=TRUE)
-# Sort out feature variables ID
-dataMatrix <- paste0("P", rownames(pos_tp0_peaklist))
-# Create dataMatrix for positive-mode data
-pos_dataMatrix <- cbind(pos_tp0_peaklist[, pos_cols])
-# Sort out rownames
-rownames(pos_dataMatrix) <- dataMatrix
-
-# Output data
-write.table(
-    pos_dataMatrix,
-    file="dataMatrix.tsv",
-    sep="\t",
-    row.names=TRUE,
-    col.names=TRUE)
-
-# Create variableMetadata for positive-mode data
-# Define columns not to extract
-meta_cols <- c("mz", "mzmin", "mzmax", "rt", "rtmin", "rtmax")
-# Sort out feature variables ID
-variableMetadata <- paste0("P", rownames(pos_tp0_peaklist))
-# Read in pre-treated positive mode data
-pos_xcms_peaklist <- read.csv(paste(output_path, "xcms_pos_peaklist.csv", sep="/"))
-# Create variableMetadata matrix for positive-mode data
-pos_variableMetadata <- cbind(pos_xcms_peaklist[rownames(pos_tp0_peaklist), meta_cols])
-# Sort out rownames
-rownames(pos_variableMetadata) <- variableMetadata
-# Output data
-write.table(
-pos_variableMetadata,
-    file="variableMetadata.tsv",
-    sep="\t",
-    row.names=TRUE,
-    col.names=TRUE)
-
-# Create sampleMetadata for positive-mode data
-# Read in metadata from file
-meta <- read.csv(paste0(datadir, "gsk/meta/meta_20180831.csv"))
-meta <- meta[which(meta[, "file_name_pos"] %in% colnames), ]
-meta_colnames <- colnames(meta)
-req_cols <- c("sample_ID", "order", "type", "block", "Regimen", "food", "exercise", "Timepoint")
-pos_sampleMetadata <- meta[, req_cols]
-# Correct column rownames
-colnames(pos_sampleMetadata)[2] <- "injectionOrder"
-colnames(pos_sampleMetadata)[3] <- "sampleType"
-colnames(pos_sampleMetadata)[4] <- "batch"
-colnames(pos_sampleMetadata)[5] <- "regimen"
-colnames(pos_sampleMetadata)[8] <- "timepoint"
-mode <- replicate(nrow(pos_sampleMetadata), "pos")
-pos_sampleMetadata <- cbind(pos_sampleMetadata, mode)
-colnames(meta)[1] <- "sampleMetadata"
-rownames(pos_sampleMetadata) <- meta[,"file_name_pos"]
-# Output data
-write.table(
-    pos_sampleMetadata,
-    file="sampleMetadata.tsv",
-    sep="\t",
-    row.names=TRUE,
-    col.names=TRUE)
+# Read in data
+pos_dataMatrix <- read.table(file=paste(output_path, "dataMatrix.tsv", sep="/"), header=TRUE, row.names=1)
+rownames(pos_dataMatrix) <- pos_dataMatrix[,1]
+pos_dataMatrix <- pos_dataMatrix[, 2:length(pos_dataMatrix)]
+pos_variableMetadata <- read.table(file=paste(output_path, "variableMetadata.tsv", sep="/"), header=TRUE, row.names=1)
+pos_sampleMetadata <- read.table(file=paste(output_path, "sampleMetadata.tsv", sep="/"), header=TRUE, row.names=1)
 
 #' Calculate means for multiple groups
 #'
@@ -83,8 +26,8 @@ write.table(
 #' @return The matrix of meanvalues for each factor represented in tp5RegARegB_regimen_fac
 #' @examples
 #' calcMean(10, 1)
-calcMean <- function(x, tp5RegARegB_regimen_fac) {
-    tapply(x, tp5RegARegB_regimen_fac, mean)
+calcMean <- function(x, fac) {
+    tapply(x, fac, mean)
 }
 
 #' Calculate means for multiple groups
@@ -94,8 +37,8 @@ calcMean <- function(x, tp5RegARegB_regimen_fac) {
 #' @return The matrix of mean values calculated for each factor represented in tp5RegARegB_regimen_fac
 #' @examples
 #' calcMean(10, 1)
-calcStdDev <- function(x, tp5RegARegB_regimen_fac) {
-    tapply(x, tp5RegARegB_regimen_fac, sd)
+calcStdDev <- function(x, fac) {
+    tapply(x, fac, sd)
 }
 
 #' Calculate t-test for multiple groups
@@ -105,9 +48,9 @@ calcStdDev <- function(x, tp5RegARegB_regimen_fac) {
 #' @return The matrix of mean values calculated for each factor represented in tp5RegARegB_regimen_fac
 #' @examples
 #' calcMean(10, 1)
-performTTest <- function(x, tp5RegARegB_regimen_fac) {
+performTTest <- function(x, fac) {
     # Need to split data based on the 2 factor groups
-    grp_data <- split(x, tp5RegARegB_regimen_fac)
+    grp_data <- split(x, fac)
     ttest <- t.test(grp_data[[1]], grp_data[[2]])
     return(ttest$p.value)
 }
