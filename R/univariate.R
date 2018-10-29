3e241339f88d80906f202ef0377cb5cd8cbaad16
Peter Li <peter@gigasciencejournal.com>
2018-10-29 15:50:14 +0800

    Plot multiple graphs in a single figure

--- R/univariate.R
+++ R/univariate.R
@@ -5,6 +5,7 @@
 
 library(ggfortify)
 library(ggplot2)
+library(cowplot)
 
 source("functions.R")
 
@@ -111,7 +112,12 @@ for(i in 1:length(tp_levs)) {
 }
 
 # Transform data into tidyr format for plotting as line graph using ggplot2
-tidyr_data <- data.frame(peak=character(), regimen=character(), tp=character(), mean=numeric(), sd=numeric(), stringsAsFactors=FALSE)
+tidyr_data <- data.frame(peak=character(),
+    regimen=character(),
+    tp=character(),
+    mean=numeric(),
+    sd=numeric(),
+    stringsAsFactors=TRUE)
 
 for(i in 1:length(rownames(tp_RegA_means))) {
     peak_name <- rownames(tp_RegA_means)[i]
@@ -131,32 +137,11 @@ for(i in 1:length(rownames(tp_RegB_means))) {
     }
 }
 
-# t_tp_RegA_means <- t(tp_RegA_means)
-# t_tp_RegA_stdevs <- t(tp_RegA_stdevs)
-# # Combine data for RegA P210
-# d1 <- cbind(t_tp_RegA_means[,1], t_tp_RegA_stdevs[,1])
-# colnames(d1) <- c("mean", "sd")
-#
-# t_tp_RegB_means <- t(tp_RegB_means)
-# t_tp_RegB_stdevs <- t(tp_RegB_stdevs)
-# # Combine data for RegB P210
-# d2 <- cbind(t_tp_RegB_means[,1], t_tp_RegB_stdevs[,1])
-# colnames(d2) <- c("mean", "sd")
-#
-# regimen <- c(rep("A", 8), rep("B", 8))
-# regimen <- factor(regimen, labels=c("A", "B"))
-#
-# tp <- c(0, 1, 4, 5, 8, 12, 16, 24, 0, 1, 4, 5, 8, 12, 16, 24)
-# tp <- factor(tp, labels=c("TP0", "TP1", "TP4", "TP5", "TP8", "TP12", "TP16", "TP24"))
-#
-# d <- rbind(d1, d2)
-# rownames(d) <- 1:nrow(d)
-# d <- data.frame(tp, d, regimen)
-
 # Error bars overlap so use position_dodge to move them horizontally
 pd <- position_dodge(0.1)  # Move them .05 to the left and right
 
-p1 <- ggplot(d, aes(x=tp, y=mean, color=regimen, group=regimen)) +
+# Use subset function to display only P210 data
+p1 <- ggplot(subset(tidyr_data, peak %in% c("P210")), aes(x=timepoint, y=mean, color=regimen, group=regimen)) +
     geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), color="black", width=.1, position=pd) +
     geom_line(position=pd) +
     geom_point(position=pd, size=3, shape=21, fill="white") +
@@ -173,4 +158,125 @@ p1 <- ggplot(d, aes(x=tp, y=mean, color=regimen, group=regimen)) +
     theme(legend.justification=c(1,0),
     legend.position=c(1,0))                 # Position legend in bottom right
 
+p2 <- ggplot(subset(tidyr_data, peak %in% c("P257")), aes(x=timepoint, y=mean, color=regimen, group=regimen)) +
+    geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), color="black", width=.1, position=pd) +
+    geom_line(position=pd) +
+    geom_point(position=pd, size=3, shape=21, fill="white") +
+    xlab('Time Point') +
+    ylab('P210') +
+    scale_colour_hue(name="Regimen",        # Legend label, use darker colors
+    breaks=c("A", "B"),
+    labels=c("A", "B"),
+    l=40) +                                 # Use darker colors, lightness=40
+    ggtitle("Compare regimens A and B") +
+    expand_limits(y=0.055) +                # Expand y range
+    scale_y_continuous(breaks=0:20*4) +     # Set tick every 4
+    theme_bw() +
+    theme(legend.justification=c(1,0),
+    legend.position=c(1,0))                 # Position legend in bottom right
+
+# Save last ggplot object
 ggsave("ggplot.pdf")
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+    library(grid)
+
+    # Make a list from the ... arguments and plotlist
+    plots <- c(list(...), plotlist)
+
+    numPlots = length(plots)
+
+    # If layout is NULL, then use 'cols' to determine layout
+    if (is.null(layout)) {
+        # Make the panel
+        # ncol: Number of columns of plots
+        # nrow: Number of rows needed, calculated from # of cols
+        layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+        ncol = cols, nrow = ceiling(numPlots/cols))
+    }
+
+    if (numPlots==1) {
+        print(plots[[1]])
+
+    } else {
+        # Set up the page
+        grid.newpage()
+        pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+
+        # Make each plot, in the correct location
+        for (i in 1:numPlots) {
+            # Get the i,j matrix positions of the regions that contain this subplot
+            matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+
+            print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+            layout.pos.col = matchidx$col))
+        }
+    }
+}
+
+pdf("plots.pdf")
+multiplot(p1, p2, cols=4)
+dev.off()
+
+peaks <- unique(tidyr_data$peak)
+plot_names <- paste0(peaks, "_plot")
+
+for(i in 1:length(peaks)) {
+    thePeak <- peaks[i]
+    # Create object name for this plot
+    plot_name <- paste0(thePeak, "_plot")
+
+    aPlot <- ggplot(subset(tidyr_data, peak %in% c(as.character(thePeak))),
+        aes(x=timepoint, y=mean, color=regimen, group=regimen)) +
+        geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd), color="black", width=.1, position=pd) +
+        geom_line(position=pd) +
+        geom_point(position=pd, size=2, shape=21, fill="white") +
+        xlab('Time Point') +
+        ylab(thePeak) +
+        scale_colour_hue(name="Regimen",        # Legend label, use darker colors
+            breaks=c("A", "B"),
+            labels=c("A", "B"),
+            l=40) +                                 # Use darker colors, lightness=40
+        # ggtitle("Compare regimens A and B") +
+        expand_limits(y=0.055) +                # Expand y range
+        scale_y_continuous(breaks=0:20*4) +     # Set tick every 4
+        theme_bw() +
+        # theme(legend.justification=c(1,0),
+        #       legend.title = element_blank(),
+        #       legend.text = element_text(size=8),
+        #       legend.box = "horizontal",
+        #       legend.position=c(1,0))         # Position legend in bottom right
+        theme(legend.position="none",           # Do not display legend
+              axis.title.x=element_blank(),     # Do not display x axis title
+              axis.text.x = element_text(size=6))
+    assign(plot_name, aPlot)
+}
+
+# Get the objects assocoiated with the object string names
+l <- mget(plot_names)
+
+# Plot graphs
+pdf("plots3.pdf")
+multiplot(plotlist=l, cols=3)
+dev.off()
+
+# Using cowplot to display multiple graphs
+graphs <- list(P210_plot, P257_plot, P439_plot, P478_plot, P727_plot, P815_plot, P816_plot, P884_plot, P988_plot, P1051_plot, P2554_plot, P2617_plot, P4141_plot, P4390_plot, P4402_plot, P4514_plot, P4518_plot, P4625_plot, P4727_plot, P4732_plot, P4736_plot, P4993_plot, P6073_plot)
+# plot2by2 <- plot_grid(P210_plot, P257_plot, P439_plot, P478_plot, ncol=2)
+plot2by2 <- plot_grid(plotlist=graphs, ncol=2, scale = c(.5, .5, .5, .5))
+save_plot("plot2by2.png", plot2by2,
+    ncol = 2, # we're saving a grid plot of 2 columns
+    nrow = 2, # and 2 rows
+    # each individual subplot should have an aspect ratio of 1.3
+    base_aspect_ratio = 1.3
+)
