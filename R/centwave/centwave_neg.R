18b3f009d5dfe4c1be4dc1ede0655328589fb1d6
Peter Li <peter@gigasciencejournal.com>
2018-11-25 12:45:56 +0800

    Update scripts

--- R/centwave/centwave_neg.R
+++ R/centwave/centwave_neg.R
@@ -3,6 +3,8 @@
 # Created by: peterli
 # Created on: 16/11/2018
 
+# https://bioconductor.org/packages/release/bioc/vignettes/xcms/inst/doc/xcms.html
+
 library(xcms)
 library(RColorBrewer)
 library(pander)
@@ -58,18 +60,20 @@ length(raw_data)
 # Look at retention time values
 head(rtime(raw_data))
 
-# Organise mx values by file
+# Organise mz values by file
 mzs <- mz(raw_data)
 mzs_by_file <- split(mzs, f = fromFile(raw_data))  # Split the list by file
 length(mzs_by_file)
 
-## Get the base peak chromatograms. This reads data from the files.
+# Plot base peak chromatogram for each file. aggregationFun is set to "max" to
+# return for each spectrum the maximal intensity thereby creating the BPC from
+# raw data
 bpis <- chromatogram(raw_data, aggregationFun = "max")
-## Define colors for the two groups
+# Define colors for the 4 block groups
 group_colors <- paste0(brewer.pal(4, "Set1")[1:4], "60")
 names(group_colors) <- c("block1", "block2", "block3", "block4")
 
-## Plot all chromatograms - this is the total ion chromatogram
+## Plot all chromatograms from the files
 pdf("./centwave/bpc_plot.pdf", width = 20, height = 8)
 plot(bpis, col = group_colors[raw_data$sample_group])
 dev.off()
@@ -85,19 +89,19 @@ pdf("./centwave/tic_plot.pdf", width = 20, height = 8)
 plot(tis, col = group_colors[raw_data$sample_group])
 dev.off()
 
-# Extract chromatogram of first sample and access its retention time and
-# intensity values
+# Extract chromatogram of first sample
 bpi_1 <- bpis[1, 1]
+# Access its retention time
 head(rtime(bpi_1))
 ## F1.S0001 F1.S0002 F1.S0003 F1.S0004 F1.S0005 F1.S0006
 ##   0.3672   0.5241   1.1115   1.7150   2.3154   2.8934
-
+# Access its intensity values
 head(intensity(bpi_1))
 ## F1.S0001 F1.S0002 F1.S0003 F1.S0004 F1.S0005 F1.S0006
 ##     8278  8567647  7064262  6896613  9335845  6393502
 
-# Create boxplots representing the distribution of total ion currents per file.
-# Such plots can be very useful to spot problematic or failing MS runs
+# Create boxplots representing distribution of total ion currents per file.
+# These plots are useful to spot problematic or failing MS runs.
 tc <- split(tic(raw_data), f = fromFile(raw_data))
 pdf("./centwave/boxplot.pdf")
 boxplot(tc, col = group_colors[raw_data$sample_group], ylab = "intensity", main = "Total ion current")
@@ -108,65 +112,88 @@ dev.off()
 #### Chromatographic peak detection ####
 ########################################
 
-# Detect chromatogrpahic peaks with noise set to 500 to reduce run time
+# Look at a single peak for a compound to check centWave parameter settings.
+# The peak looks strange with our orbitrap data.
+rtr <- c(34.0, 46.0)
+mzr <- c(238.5, 238.9)
+# Extract the chromatogram
+chr_raw <- chromatogram(raw_data, mz = mzr, rt = rtr)
+pdf("./centwave/peak.pdf")
+plot(chr_raw, col = group_colors[chr_raw$sample_group])
+dev.off()
+
+raw_data %>%
+    filterRt(rt = rtr) %>%
+    filterMz(mz = mzr) %>%
+    plot(type = "XIC")
+
+# Perform chromatographic peak detection using centWave algorithm. Noise
+# parameter is set to 5000 to reduce run time
 cwp <- CentWaveParam(ppm=20, peakwidth = c(7, 25), noise = 5000)
 xdata <- findChromPeaks(raw_data, param = cwp, BPPARAM = bpparam())
 
-# results from the chromatographic peak detection can be accessed with the chromPeaks
-# method.
+# Access results with chromPeaks method. The returned matrix provides the m/z
+# and retention time range for each identified chromatographic peak as well as
+# the integrated signal intensity (“into”) and the maximal peak intensitity
+# (“maxo”). Columns “sample” contains the index of the sample in the
+# object/experiment in which the peak was identified.
 head(chromPeaks(xdata))
 
 # Summarise results per file
 summary_fun <- function(z) {
-    c(peak_count = nrow(z), rt = quantile(z[, "rtmax"] - z[, "rtmin"]))
-}
+    c(peak_count = nrow(z), rt = quantile(z[, "rtmax"] - z[, "rtmin"]))}
 T <- lapply(split.data.frame(chromPeaks(xdata),
-                             f = chromPeaks(xdata)[, "sample"]),
-            FUN = summary_fun)
+    f = chromPeaks(xdata)[, "sample"]),
+    FUN = summary_fun)
 T <- do.call(rbind, T)
 rownames(T) <- basename(fileNames(xdata))
 pandoc.table(T,
-             caption = paste0("Summary statistics on identified chromatographic",
-                              " peaks. Shown are number of identified peaks per",
-                              " sample and widths/duration of chromatographic ",
-                              "peaks."))
+    caption = paste0("Summary statistics on identified chromatographic",
+    " peaks. Shown are number of identified peaks per",
+    " sample and widths/duration of chromatographic ",
+    "peaks."))
 
-# plot the location of the identified chromatographic peaks in the m/z -
-# retention time space for one file using the plotChromPeaks function. Below we
-# plot the chromatographic peaks for the 3rd sample.
+# Plot location of identified chromatographic peaks in the m/z - retention time
+# space for one file using the plotChromPeaks function for 3rd sample
+pdf("./centwave/location_peaks_3rdsample.pdf")
 plotChromPeaks(xdata, file = 3)
+dev.off()
 
-# get a global overview of the peak detection we can plot the frequency of
-# identified peaks per file along the retention time axis. This allows to
+# Get global overview of the peak detection by plotting frequency of identified
+# peaks per file along the retention time axis. This allows to
 # identify time periods along the MS run in which a higher number of peaks was
 # identified and evaluate whether this is consistent across files.
+pdf("./centwave/peak_freq_per_file.pdf")
 plotChromPeakImage(xdata)
+dev.off()
 
-# highlight the identified chromatographic peaks for the example peak from
-# before. Evaluating such plots on a list of peaks corresponding to known peaks
-# or internal standards helps to ensure that peak detection settings were
+# Highlight identified chromatographic peaks for example peak from before.
+# Evaluating such plots on a list of peaks corresponding to known peaks or
+# internal standards helps to ensure that peak detection settings were
 # appropriate and correctly identified the expected peaks.
+pdf("./centwave/highlight_peak.pdf")
 plot(chr_raw, col = group_colors[chr_raw$sample_group], lwd = 2)
 highlightChromPeaks(xdata,
     border = group_colors[chr_raw$sample_group],
     lty = 3, rt = rtr, mz = mzr, type = "rect")
+dev.off()
 
 # specifically extract identified chromatographic peaks for a selected region by
 # providing the respective m/z and retention time ranges with the mz and rt
 # arguments in the chromPeaks method.
 pander(chromPeaks(xdata, mz = mzr, rt = rtr),
 caption = paste("Identified chromatographic peaks in a selected ",
-"m/z and retention time range."))
+    "m/z and retention time range."))
 
-# plot also the distribution of peak intensity per sample. This allows to
-# investigate whether systematic differences in peak signals between samples are
-# present.
-## Extract a list of per-sample peak intensities (in log2 scale)
-ints <- split(log2(chromPeaks(xdata)[, "into"]),
-f = chromPeaks(xdata)[, "sample"])
+# Plot distribution of peak intensity per sample to check whether systematic
+# differences in peak signals between samples are present.
+pdf("./centwave/peak_intensity_distribution.pdf")
+# Extract a list of per-sample peak intensities (in log2 scale)
+ints <- split(log2(chromPeaks(xdata)[, "into"]), f = chromPeaks(xdata)[, "sample"])
 boxplot(ints, varwidth = TRUE, col = group_colors[xdata$sample_group],
-ylab = expression(log[2]~intensity), main = "Peak intensities")
+    ylab = expression(log[2]~intensity), main = "Peak intensities")
 grid(nx = NA, ny = NULL)
+dev.off()
 
 
 ###################
@@ -175,24 +202,24 @@ grid(nx = NA, ny = NULL)
 
 # Analytes can elute at different times between samples during chromatography.
 # Retention time correction can align same peak signals between different
-# samples within an experiment. USe adjestRtime function to perform RT
-# correction
+# samples within an experiment. Use adjustRtime function to perform RT
+# correction to align signals between samples.
 xdata <- adjustRtime(xdata, param = ObiwarpParam(binSize = 0.6))
 
-## Extract adjusted retention times
+# Extract adjusted retention times
 head(adjustedRtime(xdata))
 
-# To evaluate the impact of the alignment we plot the BPC on the adjusted data
-## Get the base peak chromatograms.
+# Plot BPC on adjusted data to evaluate impact of signal alignment using RT
+# Result looks strange and some runs look truncated
 bpis_adj <- chromatogram(xdata, aggregationFun = "max")
 par(mfrow = c(2, 1), mar = c(4.5, 4.2, 1, 0.5))
-pdf("./centwave/BPC_adjustedRT.pdf")
+pdf("./centwave/BPC_adjustedRT.pdf", width = 20, height = 8)
 plot(bpis_adj, col = group_colors[bpis_adj$sample_group])
 dev.off()
 
-## Plot also the difference of adjusted to raw retention time.
-# Too large differences between adjusted and raw retention times could indicate
-# poorly performing samples or alignment.
+# Plot difference of adjusted to raw retention time. Large differences between
+# adjusted and raw retention times could indicate poorly performing samples or
+# alignment
 pdf("./centwave/BPC_diff_adjusted_rawRT.pdf")
 plotAdjustedRtime(xdata, col = group_colors[xdata$sample_group])
 dev.off()
@@ -221,10 +248,9 @@ plotAdjustedRtime(xdata, col = group_colors[xdata$sample_group],
 peakGroupsCol = "grey", peakGroupsPch = 1)
 dev.off()
 
-# At last we evaluate the impact of the alignment on the test peak.
+# Evaluate impact of RT alignment on test peak
 pdf("./centwave/single_peak.pdf")
 par(mfrow = c(2, 1))
-## Plot the raw data
 plot(chr_raw, col = group_colors[chr_raw$sample_group])
 dev.off()
 
